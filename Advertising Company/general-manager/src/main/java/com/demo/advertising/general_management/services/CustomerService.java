34ed414abedaca6cb21136a272c63cb6ba9584b4
amylauren5 <amy.abela.21@um.edu.mt>
2022-12-21 17:38:24 +0100

    more changes to pom file

--- Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/services/CustomerService.java
+++ Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/services/CustomerService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class CustomerService {
