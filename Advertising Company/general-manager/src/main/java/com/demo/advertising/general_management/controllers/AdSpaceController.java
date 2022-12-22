4f43da2a75d9476bba5a08f08f8e8467b1c8a238
jasmica140 <98131692+jasmica140@users.noreply.github.com>
2022-12-22 22:24:27 +0100

    GetAdSpace changed

--- Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/AdSpaceController.java
+++ Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/AdSpaceController.java
@@ -31,17 +31,11 @@ public class AdSpaceController {
     }
 
     @GetMapping(path = "{spaceId}")
-    public ResponseEntity<GetAdSpaceResponse> get(String spaceId) {
-
-        Adspace adspace = adSpaceService.getAdSpace(spaceId);
-
-        if (adspace == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        GetAdSpaceResponse getAdSpaceResponse = mapper.map(adspace, GetAdSpaceResponse.class);
-        return ResponseEntity.ok(getAdSpaceResponse);
+    public Adspace get(String spaceId){
+        return adSpaceService.getAdSpace(spaceId);
     }
+
+
 
     @PutMapping(path = "{spaceId}")
     public void updateAdSpace(@PathVariable("spaceId") String spaceId, @RequestBody Adspace adspace) {
