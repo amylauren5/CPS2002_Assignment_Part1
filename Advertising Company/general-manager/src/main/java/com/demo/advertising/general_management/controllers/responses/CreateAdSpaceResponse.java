22d2fe776f5ff5886afbf6cb79ea8cef491bbe47
jasmica140 <98131692+jasmica140@users.noreply.github.com>
2022-12-23 23:05:37 +0100

    GetAdSpace changed

--- Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/responses/CreateAdSpaceResponse.java
+++ Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/responses/CreateAdSpaceResponse.java
@@ -1,7 +1,5 @@
 package com.demo.advertising.general_management.controllers.responses;
 
-import com.demo.advertising.general_management.services.models.Adspace;
-
 public class CreateAdSpaceResponse {
 
     private String SpaceId;
--- Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/responses/GetAdSpaceResponse.java
+++ Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/responses/GetAdSpaceResponse.java
@@ -3,23 +3,25 @@ import com.demo.advertising.general_management.services.models.Adspace;
 
 public class GetAdSpaceResponse {
 
-    private Adspace adspace;
     private String SpaceId;
+    private int Popularity;     //people viewing space per day
+    private String Type;        //billboard, buses, benches
+    private int Size;           //size of advertising space
+    private int Price;          //cost to rent space per week
+    private String Location;    //town/park where ad space is
+    private int BusRoute;       //bus route
 
-    public GetAdSpaceResponse(Adspace adspace, String SpaceId) {
-        this.adspace = adspace;
+    public GetAdSpaceResponse(String SpaceId, int Popularity, String Type, int Size, int Price, String Location, int BusRoute) {
         this.SpaceId = SpaceId;
+        this.Popularity = Popularity;
+        this.Type = Type;
+        this.Size = Size;
+        this.Price = Price;
+        this.Location = Location;
+        this.BusRoute = BusRoute;
     }
 
     public GetAdSpaceResponse() {
-    }
-
-    public Adspace getAdspace() {
-        return adspace;
-    }
-
-    public void setAdspace(Adspace adspace) {
-        this.adspace = adspace;
     }
 
     public String getSpaceId() {
@@ -28,5 +30,53 @@ public class GetAdSpaceResponse {
 
     public void setSpaceId(String spaceId) {
         SpaceId = spaceId;
+    }
+
+    public int getPopularity() {
+        return Popularity;
+    }
+
+    public void setPopularity(int popularity) {
+        Popularity = popularity;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public int getSize() {
+        return Size;
+    }
+
+    public void setSize(int size) {
+        Size = size;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public int getBusRoute() {
+        return BusRoute;
+    }
+
+    public void setBusRoute(int busRoute) {
+        BusRoute = busRoute;
     }
 }
