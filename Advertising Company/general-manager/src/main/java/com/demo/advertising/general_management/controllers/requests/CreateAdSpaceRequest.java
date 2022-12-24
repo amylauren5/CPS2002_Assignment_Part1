4d31c1920b3bdb22a3272226f0381c68dcdc1ea9
jasmica140 <98131692+jasmica140@users.noreply.github.com>
2022-12-24 17:10:11 +0100

    GetAdSpace changed

--- Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/requests/CreateAdSpaceRequest.java
+++ Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/requests/CreateAdSpaceRequest.java
@@ -3,70 +3,21 @@ package com.demo.advertising.general_management.controllers.requests;
 import java.util.List;
 
 public class CreateAdSpaceRequest {
-    private int Popularity;     //people viewing space per day
-    private String Type;        //billboard, buses, benches
-    private int Size;           //size of advertising space
-    private int Price;          //cost to rent space per week
-    private String Location;    //town/park where ad space is
-    private int BusRoute;       //bus route
+    private AdSpace adSpace;
 
-    public CreateAdSpaceRequest(int Popularity, String Type, int Size, int Price, String Location, int BusRoute) {
-        this.Popularity = Popularity;
-        this.Type = Type;
-        this.Size = Size;
-        this.Price = Price;
-        this.Location = Location;
-        this.BusRoute = BusRoute;
+    public CreateAdSpaceRequest(AdSpace adSpace) {
+        this.adSpace = adSpace;
     }
+
     public CreateAdSpaceRequest() {
     }
 
-    public int getPopularity() {
-        return Popularity;
+    public AdSpace getAdSpace() {
+        return adSpace;
     }
 
-    public void setPopularity(int popularity) {
-        Popularity = popularity;
-    }
-
-    public String getType() {
-        return Type;
-    }
-
-    public void setType(String type) {
-        Type = type;
-    }
-
-    public int getSize() {
-        return Size;
-    }
-
-    public void setSize(int size) {
-        Size = size;
-    }
-
-    public int getPrice() {
-        return Price;
-    }
-
-    public void setPrice(int price) {
-        Price = price;
-    }
-
-    public String getLocation() {
-        return Location;
-    }
-
-    public void setLocation(String location) {
-        Location = location;
-    }
-
-    public int getBusRoute() {
-        return BusRoute;
-    }
-
-    public void setBusRoute(int busRoute) {
-        BusRoute = busRoute;
+    public void set(AdSpace adSpace) {
+        this.adSpace = adSpace;
     }
 
 }
