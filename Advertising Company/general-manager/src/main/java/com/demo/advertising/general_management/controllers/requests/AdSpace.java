d152873ad6590ebec5e3ba89f25c14a9bc67c8ec
jasmica140 <98131692+jasmica140@users.noreply.github.com>
2022-12-26 14:50:18 +0100

    GetAdSpace changed

--- Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/requests/AdSpace.java
+++ Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/requests/AdSpace.java
@@ -2,72 +2,22 @@ package com.demo.advertising.general_management.controllers.requests;
 
 public class AdSpace {
 
-    private int Popularity;     //people viewing space per day
-    private String Type;        //billboard, buses, benches
-    private int Size;           //size of advertising space
-    private int Price;          //cost to rent space per week
-    private String Location;    //town/park where ad space is
-    private int BusRoute;       //bus route
+    private AdSpace adSpace;
 
 
-    public AdSpace(int Popularity, String Type, int Size, int Price, String Location, int BusRoute) {
-        this.Popularity = Popularity;
-        this.Type = Type;
-        this.Size = Size;
-        this.Price = Price;
-        this.Location = Location;
-        this.BusRoute = BusRoute;
+    public AdSpace(AdSpace adSpace) {
+        this.adSpace = adSpace;
     }
 
     public AdSpace(){
     }
 
-    public int getPopularity() {
-        return Popularity;
+    public AdSpace getAdSpace() {
+        return adSpace;
     }
 
-    public void setPopularity(int Popularity) {
-        this.Popularity = Popularity;
-    }
-
-    public String getType() {
-        return Type;
-    }
-
-    public void setType(String Type) {
-        this.Type = Type;
-    }
-
-    public int getSize() {
-        return Size;
-    }
-
-    public void setSize(int Size) {
-        this.Size = Size;
-    }
-
-    public int getPrice() {
-        return Price;
-    }
-
-    public void setPrice(int Price) {
-        this.Price = Price;
-    }
-
-    public String getLocation() {
-        return Location;
-    }
-
-    public void setLocation(String Location) {
-        this.Location = Location;
-    }
-
-    public int getBusRoute() {
-        return BusRoute;
-    }
-
-    public void setBusRoute(int BusRoute) {
-        this.BusRoute = BusRoute;
+    public void setAdSpace(AdSpace adSpace) {
+        this.adSpace = adSpace;
     }
 
 }
--- Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/responses/GetAdSpaceResponse.java
+++ Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/responses/GetAdSpaceResponse.java
@@ -3,80 +3,21 @@ import com.demo.advertising.general_management.services.models.Adspace;
 
 public class GetAdSpaceResponse {
 
-    private String SpaceId;
-    private int Popularity;     //people viewing space per day
-    private String Type;        //billboard, buses, benches
-    private int Size;           //size of advertising space
-    private int Price;          //cost to rent space per week
-    private String Location;    //town/park where ad space is
-    private int BusRoute;       //bus route
+    private Adspace adSpace;
 
-    public GetAdSpaceResponse(String SpaceId, int Popularity, String Type, int Size, int Price, String Location, int BusRoute) {
-        this.SpaceId = SpaceId;
-        this.Popularity = Popularity;
-        this.Type = Type;
-        this.Size = Size;
-        this.Price = Price;
-        this.Location = Location;
-        this.BusRoute = BusRoute;
+    public GetAdSpaceResponse(Adspace adSpace) {
+        this.adSpace = adSpace;
     }
 
     public GetAdSpaceResponse() {
     }
 
-    public String getSpaceId() {
-        return SpaceId;
+    public Adspace getAdSpace() {
+        return adSpace;
     }
 
-    public void setSpaceId(String spaceId) {
-        SpaceId = spaceId;
+    public void setAdSpace(Adspace adSpace) {
+        this.adSpace = adSpace;
     }
 
-    public int getPopularity() {
-        return Popularity;
-    }
-
-    public void setPopularity(int popularity) {
-        Popularity = popularity;
-    }
-
-    public String getType() {
-        return Type;
-    }
-
-    public void setType(String type) {
-        Type = type;
-    }
-
-    public int getSize() {
-        return Size;
-    }
-
-    public void setSize(int size) {
-        Size = size;
-    }
-
-    public int getPrice() {
-        return Price;
-    }
-
-    public void setPrice(int price) {
-        Price = price;
-    }
-
-    public String getLocation() {
-        return Location;
-    }
-
-    public void setLocation(String location) {
-        Location = location;
-    }
-
-    public int getBusRoute() {
-        return BusRoute;
-    }
-
-    public void setBusRoute(int busRoute) {
-        BusRoute = busRoute;
-    }
 }
