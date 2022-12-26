37019afbaeb01723ceb148636b2b3c7e3e8b58de
jasmica140 <98131692+jasmica140@users.noreply.github.com>
2022-12-26 15:33:53 +0100

    GetAdSpace changed

--- Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/responses/GetAdSpaceResponse.java
+++ Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/responses/GetAdSpaceResponse.java
@@ -5,25 +5,22 @@ import com.demo.advertising.general_management.services.models.Adspace;
 public class GetAdSpaceResponse {
 
     private String SpaceId;
-    private int Popularity;     //people viewing space per day
-    private String Type;        //billboard, buses, benches
-    private int Size;           //size of advertising space
-    private int Price;          //cost to rent space per week
-    private String Location;    //town/park where ad space is
-    private int BusRoute;       //bus route
+    private Adspace adSpace;
 
-
-    public GetAdSpaceResponse(String SpaceId, int Popularity, String Type, int Size, int Price, String Location, int BusRoute) {
+    public GetAdSpaceResponse(String SpaceId, Adspace adSpace) {
         this.SpaceId = SpaceId;
-        this.Popularity = Popularity;
-        this.Type = Type;
-        this.Size = Size;
-        this.Price = Price;
-        this.Location = Location;
-        this.BusRoute = BusRoute;
+        this.adSpace = adSpace;
     }
 
-    public GetAdSpaceResponse(){
+    public GetAdSpaceResponse() {
+    }
+
+    public Adspace getAdSpace() {
+        return adSpace;
+    }
+
+    public void setAdSpace(Adspace adSpace) {
+        this.adSpace = adSpace;
     }
 
     public String getSpaceId() {
@@ -33,53 +30,4 @@ public class GetAdSpaceResponse {
     public void setSpaceId(String spaceId) {
         SpaceId = spaceId;
     }
-
-    public int getPopularity() {
-        return Popularity;
-    }
-
-    public void setPopularity(int Popularity) {
-        this.Popularity = Popularity;
-    }
-
-    public String getType() {
-        return Type;
-    }
-
-    public void setType(String Type) {
-        this.Type = Type;
-    }
-
-    public int getSize() {
-        return Size;
-    }
-
-    public void setSize(int Size) {
-        this.Size = Size;
-    }
-
-    public int getPrice() {
-        return Price;
-    }
-
-    public void setPrice(int Price) {
-        this.Price = Price;
-    }
-
-    public String getLocation() {
-        return Location;
-    }
-
-    public void setLocation(String Location) {
-        this.Location = Location;
-    }
-
-    public int getBusRoute() {
-        return BusRoute;
-    }
-
-    public void setBusRoute(int BusRoute) {
-        this.BusRoute = BusRoute;
-    }
-
 }
