7127cd157893233461aa3af70a989bab800b4d3e
jasmica140 <98131692+jasmica140@users.noreply.github.com>
2022-12-26 16:43:05 +0100

    GetAdSpace changed

--- Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/requests/CreateAdSpaceRequest.java
+++ Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/controllers/requests/CreateAdSpaceRequest.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 public class CreateAdSpaceRequest {
     private String Location;    //town/park where ad space is
+    private String Type;        //billboard, buses, benches
 
 
-    public CreateAdSpaceRequest(String Location, int BusRoute) {
+    public CreateAdSpaceRequest(String Location, String Type) {
         this.Location = Location;
+        this.Type = Type;
     }
 
     public CreateAdSpaceRequest() {
@@ -21,4 +23,11 @@ public class CreateAdSpaceRequest {
         Location = location;
     }
 
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
 }
