dadf9d3868e9b1b11e0ea732ad86d8bea5df9f79
amylauren5 <amy.abela.21@um.edu.mt>
2022-12-28 18:32:52 +0100

    removed singleton scope

--- Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/services/CustomerService.java
+++ Advertising Company/general-manager/src/main/java/com/demo/advertising/general_management/services/CustomerService.java
@@ -5,8 +5,6 @@ import com.demo.advertising.general_management.data.repositories.CustomerReposit
 import com.demo.advertising.general_management.services.models.Customer;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
@@ -16,7 +14,6 @@ import java.util.Objects;
 import java.util.Optional;
 
 @Service
-@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
